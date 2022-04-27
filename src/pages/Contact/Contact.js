7b605b20c466e14df72b20eb2da69af62ed70ec4
Teeking0007 <timmyther02@gmail.com>
2022-04-27 18:34:51 +0100

    changes to the contact page and the hero section

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -9,6 +9,26 @@ const Contact = () => {
         <Navbar/>
         <div className="contact">
             <div className="tittle"><h1>Contact</h1></div>
+            <div className="con-text">
+                
+                <div className="left-side">
+                    <h2>Pizza Palace</h2>
+                    <p>No. 49 jajaja street, Ikoyi Lagos State</p>
+                    <p>Tel.: +23470000000000</p>
+                    <p>Email: info@pizzapalace.com</p>
+                </div>
+                <div className="right-side">
+                    <h4>Opening Time</h4>
+                    <ul>
+                        <li>Monday: 6:00am - 8:00pm</li>
+                        <li>Tuesday: 6:00am - 8:00pm</li>
+                        <li>Wednesday: 6:00am - 8:00pm</li>
+                        <li>Thursday: 6:00am - 8:00pm</li>
+                        <li>Friday: 6:00am - 8:00pm</li>
+                        <li>Saturday: 6:00am - 8:00pm</li>
+                    </ul>
+                </div>
+            </div>
             <form>
                 <div>
                     <label>Enter Your Name</label>
@@ -24,7 +44,7 @@ const Contact = () => {
                 </div>
                 <div>
                     <label>Enter Your Name</label>
-                    <textarea>Message</textarea>
+                    <textarea defaultChecked >Message</textarea>
                 </div>
                 <button className="con-button">Send</button>
             </form>
